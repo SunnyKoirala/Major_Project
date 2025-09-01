1ecc8debb5489598891a2a2fcfc5c9ac1d8b55a3
Sunny Koirala <sunnykoirala777@gmail.com>
2025-09-01 15:43:54 +0545

    made some changes

--- app.js
+++ app.js
@@ -3,10 +3,11 @@ const app=express();
 const Listing=require("./models/listing");
 const mongoose=require("mongoose");
 const path=require("path");
+const methodOverride=require("method-override");
 app.set("view engine","ejs");
 app.set("views",path.join(__dirname,"views"));
 app.use(express.urlencoded({extended:true}));
-
+app.use(methodOverride("_method"));
 main().then(()=>{
     console.log("Data saved successfully");
 }).catch((err)=>{
@@ -68,6 +69,12 @@ app.get("/listings/:id/edit",async(req,res)=>{
     res.render("listings/edit",{listing});
 });
 
+//Update Route
+app.put("/listings/:id",async(req,res)=>{
+    let{id}=req.params;
+    await Listing.findByIdAndUpdate(id,{...req.body.listing});
+    res.redirect(`/listings/${id}`);
+});
 
 app.listen(8080,()=>{
     console.log("Server is listening to port no 8080");
