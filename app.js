172e29338f4a2eb037208a3dbefd8e70a62d8771
Sunny Koirala <sunnykoirala777@gmail.com>
2025-09-03 09:44:38 +0545

    made some changes

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ app.set("view engine","ejs");
 app.set("views",path.join(__dirname,"views"));
 app.use(express.urlencoded({extended:true}));
 app.use(methodOverride("_method"));
+app.engine("ejs",ejsMate);
 main().then(()=>{
     console.log("Data saved successfully");
 }).catch((err)=>{
